2d7313435269d4449b491b88d891ee3e48f95b0a
timenbob <timen.bobnar@gmail.com>
2023-12-14 10:41:22 +0100

    vaje

---
+++ vaje_14_12_2023/pridobi_podatke.py
@@ -0,0 +1,30 @@
+import requests
+import bs4
+
+
+
+html = requests.get("https://www.legendww.me/zenska-odjeca")
+
+def pridobi_ime(blok):
+    ime=blok.find_all("div",attrs={"class":"product__teaser__title"})
+    return ime.h2.string
+    
+def pridobi_ceno(blok):
+    podatki=blok.find_all("div",attrs={"class":"product__teaser__price"})
+    ceno=float(ime.div.span)
+    valuta= podatki.div.next.next_sibling.next_sibling.next_sibling 
+    return ceno,valuta
+
+juha = bs4.BeautifulSoup(html.content)
+
+with open("podatki\oblacila.csv","w", encoding="UTF-8") as dat:
+print("ime,cena,valuta",file=dat)
+for izdelek in juha.find_all("div",attrs={"class":"product__teaser"}):
+    ime=pridobi_ime(izdelek)
+    cena, valuta=pridobi_ceno(izdelek)
+    print(f"{ime},{cena},{valuta}",file=dat)
+    
+
+    
+
+
