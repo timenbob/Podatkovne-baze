9daa65b67ecec0cb8cc6d07c8a434c9903cda9a1
timenbob <timen.bobnar@gmail.com>
2024-01-04 08:11:14 +0100

    neki

--- vaje_14_12_2023/pridobi_podatke.py
+++ vaje_14_12_2023/pridobi_podatke.py
@@ -18,11 +18,12 @@ def pridobi_ceno(blok):
 juha = bs4.BeautifulSoup(html.content)
 
 with open("podatki\oblacila.csv","w", encoding="UTF-8") as dat:
-print("ime,cena,valuta",file=dat)
-for izdelek in juha.find_all("div",attrs={"class":"product__teaser"}):
-    ime=pridobi_ime(izdelek)
-    cena, valuta=pridobi_ceno(izdelek)
-    print(f"{ime},{cena},{valuta}",file=dat)
+    print("ime,cena,valuta",file=dat)
+    for izdelek in juha.find_all("div",attrs={"class":"product__teaser"}):
+        ime=pridobi_ime(izdelek)
+        cena, valuta=pridobi_ceno(izdelek)
+        
+        print(f"{ime},{cena},{valuta}",file=dat)
     
 
     
